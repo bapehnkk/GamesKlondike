4d6e6bd034bc51c6857556d110a9e4f1ab3b39d5
bapehnkk <49856345+bapehnkk@users.noreply.github.com>
2022-03-08 15:15:48 +0200

    daily
    add lazy image lolading,
    add login, register front pages
    add (not finished) profile page

--- main/front/urls.py
+++ main/front/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='index'),
     path('post/', views.post, name='post'), # условно, потом путь изменить
+    path('login/', views.login, name='login'), 
+    path('register/', views.register, name='register'), 
+    path('profile/', views.profile, name='profile'), 
+    path('profile/bookmark', views.profile_bookmark, name='profile_bookmark'), 
 ]
--- main/front/views.py
+++ main/front/views.py
@@ -15,7 +15,7 @@ def home(request):
         })
         cards.append({
             'title': 'Title'+str(n+1),
-            'link': 'post',
+            'link': '/post/',
             'text': ('Text'+str(n+1))*100,
             'date': 'date'+str(n+1),
             'tags': [{
@@ -38,4 +38,51 @@ def home(request):
 def post(request):
     dict = {}
     return render(request, 'front/post.html', dict)
+
+
+def login(request):
+    dict = {}
+    return render(request, 'front/login.html', dict)
+
+def register(request):
+    dict = {}
+    return render(request, 'front/register.html', dict)
+
+
+def profile(request):
+    dict = {}
+    return render(request, 'front/profile.html', dict)
+
+
+def profile_bookmark(request):
+    alerts = []
+    playlists = []
+    cards = []
+    for n in range(5):
+        alerts.append({
+            'text': 'Lorem'*50,
+            'time': str((n+1)*2)+'min ago'
+        })
+        playlists.append({
+            'text': n,
+        })
+        cards.append({
+            'title': 'Title'+str(n+1),
+            'link': '/post/',
+            'text': ('Text'+str(n+1))*100,
+            'date': 'date'+str(n+1),
+            'tags': [{
+                'link': '/',
+                'text': 'tag'+str(n+1),
+            },
+            {'link': '/','text': 'tag'+str(n+1),}
+            ],
+        })
+    dict = {
+        'alerts': alerts,
+        'playlists': playlists,
+        'cards': cards,
+        'profile_if': 'bookmark',
+    }
+    return render(request, 'front/profile.html', dict)
 
